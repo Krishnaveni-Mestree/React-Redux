128abb215988881de4f0827f93b703154cc0401e
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-09 12:30:24 +0530

    we got access to our disptach function  by useDispatch hook (similar functionality of mapDispatchToProps)

--- src/components/HooksContainer.js
+++ src/components/HooksContainer.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { OrderPizza } from "../redux";
 
 
 function HooksContainer(props){
     //which will access my pizzaBase available in my redux state
     const pizzaBase=useSelector(state=>state.pizzaBase)  //similar to mapStateToProps
-    console.log(pizzaBase)
+    //dispatch hook will be returning a reference to dispatch function from our redux store
+    const dispatch=useDispatch(); //similar to mapDispatchToProps
+    console.log(pizzaBase);
     return(
         <div className='container'>
             <h2 className="text">Number of Pizza Base available -{pizzaBase}</h2>
-            <button className="btn">Order Pizza</button>
+            <button className="btn" onClick={()=>dispatch(OrderPizza())}>Order Pizza</button>
         </div>
     );
 };
