1ee8f84fa9dc330b8250615711de183ef609f497
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-09 12:52:34 +0530

    defined files for BurgerBox and made changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import PizzaBox from './components/PizzaBox';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import HooksContainer from './components/HooksContainer';
+import BurgerBox from './components/BurgerBox';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        {/* <PizzaBox/> */}
-        <HooksContainer/>
+        <PizzaBox/>
+        {/* <HooksContainer/> */}
+        <BurgerBox/>
       </Provider>
     </div>
   );
---
+++ src/components/BurgerBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { OrderPizza } from "../redux/index.js";
+import { connect } from "react-redux";
+
+
+function BurgerBox(props){
+    console.log(props)
+    return(
+        <div className='container'>
+            <h2 className="text">Number of Burger Buns available - {props.pizzaBase}</h2>
+            <button className="btn" onClick={props.orderPizza}>Order Burger</button>
+        </div>
+    );
+};
+
+//1.we got access to our redux state
+const mapStateToProps=(state)=>{
+    return{
+        pizzaBase:state.pizzaBase //this prop will pass as prop to the component
+    }
+}
+
+//2.we got access to dispatch function the action from redux store
+const mapDispatchToProps=(dispatch)=>{
+    return{
+        orderPizza:()=>dispatch(OrderPizza()) //once dispatch action, hill be going inside the store
+        //this prop will pass as prop to the component
+    }
+}
+
+//3.connect both functions to our component
+export default connect(mapStateToProps,mapDispatchToProps)(BurgerBox);
