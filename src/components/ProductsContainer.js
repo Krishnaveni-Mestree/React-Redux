b68b2327ddf401a484650184582c56c597a96a60
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-09 18:41:28 +0530

    i have replaced mapStateToProps and mapDispatchToProps with useSelector and useDispatch hooks

--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -1,30 +1,37 @@
 import React, { useEffect } from 'react'
 import { fetchProducts } from '../redux';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+//import { connect } from 'react-redux';
 
-function ProductsContainer({productsData,fetchProducts}) {
+function ProductsContainer() {
+    const productsData=useSelector(state=>state.product);
+    const dispatch=useDispatch();
+
     useEffect(()=>{
-        fetchProducts()
+        //fetchProducts()
+        dispatch(fetchProducts())
     },[]);
   return (
     <div>
       {productsData.loading && <p>Loading....</p>}
       {productsData.error && <p>{productsData.error}</p>}
-      {productsData.products && <p>{productsData.products.map(title=><h1>{title}</h1>)}</p>}
+      {productsData.products && <p>{productsData.products.map(title=><p>{title}</p>)}</p>}
     </div>
   )
 };
 
-const mapStateToProps=state=>{
-    return{
-       productsData:state.product
-    }
-};
+// const mapStateToProps=state=>{
+//     return{
+//        productsData:state.product
+//     }
+// };
 
-const mapDispatchToProps=dispatch=>{
-    return{
-        fetchProducts:()=>dispatch(fetchProducts())
-    }
-}
+// const mapDispatchToProps=dispatch=>{
+//     return{
+//         fetchProducts:()=>dispatch(fetchProducts())
+//     }
+// }
 
-export default connect(mapStateToProps,mapDispatchToProps)(ProductsContainer)
+// export default connect(mapStateToProps,mapDispatchToProps)(ProductsContainer)
+
+export default ProductsContainer;
