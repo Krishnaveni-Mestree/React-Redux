744dbc3ee1b168eba77d77b8561bed0d5edc3e9f
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-09 11:44:40 +0530

    connected both mapStateToProps and mapDispatchToProps by connect func. - 3rd step

--- src/components/PizzaBox.js
+++ src/components/PizzaBox.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { OrderPizza } from "../redux/index.js";
+import { connect } from "react-redux";
+
 
 function PizzaBox(props){
     console.log(props)
     return(
         <div className='container'>
-            <h2 className="text">Number of Pizza Base available - 100</h2>
-            <button className="btn">Order Pizza</button>
+            <h2 className="text">Number of Pizza Base available - {props.pizzaBase}</h2>
+            <button className="btn" onClick={props.orderPizza}>Order Pizza</button>
         </div>
     );
 };
@@ -14,15 +16,17 @@ function PizzaBox(props){
 //1.we got access to our redux state
 const mapStateToProps=(state)=>{
     return{
-        pizzaBase:state.pizzaBase
+        pizzaBase:state.pizzaBase //this prop will pass as prop to the component
     }
 }
 
-//2.we got access to dispatching the action from redux store
+//2.we got access to dispatch function the action from redux store
 const mapDispatchToProps=(dispatch)=>{
     return{
-        orderPizza:()=>dispatch(OrderPizza())
+        orderPizza:()=>dispatch(OrderPizza()) //once dispatch action, hill be going inside the store
+        //this prop will pass as prop to the component
     }
 }
 
-export default PizzaBox;
+//3.connect both functions to our component
+export default connect(mapStateToProps,mapDispatchToProps)(PizzaBox);
